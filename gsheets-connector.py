7364021460f5d9aeb50864bf9fb0277d4f75353c
pealock <andrewpealock@gmail.com>
2025-01-14 20:24:07 -0500

    init

---
+++ gsheets-connector.py
@@ -0,0 +1,20 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+# Define the scope for authentication
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+
+# Authenticate using the service account credentials
+credentials = ServiceAccountCredentials.from_json_keyfile_name('./.creds/sheets-python-447900-cff818541424.json', scope)
+
+# Authenticate the gspread client
+client = gspread.authorize(credentials)
+
+# Open the Google Sheet by its name or URL
+sheet = client.open('python-sheets').sheet1
+
+# Get all values from the sheet
+data = sheet.acell('F1').numeric_value
+
+# Print the data
+print(data)
