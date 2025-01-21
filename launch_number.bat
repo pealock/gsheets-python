23389d2a9b901771eccca4c84b92be9a1569ed7e
pealock <andrewpealock@gmail.com>
2025-01-21 08:48:17 -0500

    removed launch LED app... for now

--- launch_number.bat
+++ launch_number.bat
@@ -4,9 +4,6 @@ MODE 50,10
 REM connect to wifi
 REM netsh wlan connect ssid="" name="My Name"
 
-REM Launch LED control app
-start "C:\Users\BA Admin\Documents\ÈðºÏÐÅPlus V8.0°æ\ÈðºÏÐÅPlus.exe"
-
 REM cd to git repo directory and launch script
 cd %HOMEPATH%\Desktop\pealock\gsheets-python
 python ./number.py
