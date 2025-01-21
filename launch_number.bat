10845273f3622a60e123bc94a87c82a088703b55
pealock <andrewpealock@gmail.com>
2025-01-21 08:30:45 -0500

    comments for context

--- launch_number.bat
+++ launch_number.bat
@@ -1,3 +1,9 @@
+REM set window size
 MODE 50,10
+
+REM connect to wifi
+REM netsh wlan connect ssid="" name="My Name"
+
+REM cd to git repo directory and launch script
 cd %HOMEPATH%\Desktop\pealock\gsheets-python
 python ./number.py
