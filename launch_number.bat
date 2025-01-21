af8af0007b5ddc35284a0785583d5ceb00660549
pealock <andrewpealock@gmail.com>
2025-01-21 08:43:09 -0500

    added command to launch LED add

--- launch_number.bat
+++ launch_number.bat
@@ -4,6 +4,9 @@ MODE 50,10
 REM connect to wifi
 REM netsh wlan connect ssid="" name="My Name"
 
+REM Launch LED control app
+start "C:\Users\BA Admin\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\ÈðºÏÐÅPlus V8.0°æ\ÈðºÏÐÅPlus.lnk"
+
 REM cd to git repo directory and launch script
 cd %HOMEPATH%\Desktop\pealock\gsheets-python
 python ./number.py
